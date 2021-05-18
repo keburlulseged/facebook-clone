b9ea8c7b6cf9ef5f073496c91af494bd20eb12ad
Kebur Lulseged <kebur.lulseged@gmail.com>
2021-05-18 10:12:50 -0700

    add image upload functionality to input box

--- components/InputBox.js
+++ components/InputBox.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import { session, useSession } from "next-auth/client";
 import Image from "next/image";
 import { EmojiHappyIcon } from "@heroicons/react/outline";
@@ -9,6 +9,8 @@ import firebase from "firebase";
 function InputBox() {
   const [session] = useSession();
   const inputRef = useRef(null);
+  const fileRef = useRef(null);
+  const [imageToPost, setImageToPost] = useState(null);
 
   const sendPost = (e) => {
     e.preventDefault();
@@ -23,6 +25,21 @@ function InputBox() {
     });
 
     inputRef.current.value = "";
+  };
+
+  const addImageToPost = (e) => {
+    const reader = new FileReader();
+    if (e.target.files[0]) {
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
+    reader.onload = (readerEvent) => {
+      setImageToPost(readerEvent.target.result);
+    };
+  };
+
+  const removeImage = () => {
+    setImageToPost(null);
   };
 
   return (
@@ -46,6 +63,13 @@ function InputBox() {
             Submit
           </button>
         </form>
+
+        {imageToPost && (
+          <div onClick={removeImage} className="flex flex-col filter hover:brightness-110 transition duration-150 transform hover:scale-105 cursor-pointer">
+            <img className="h-10 object-contain" src={imageToPost} alt="" />
+            <p className="text-xs text-red-500 text-center">Remove</p>
+          </div>
+        )}
       </div>
 
       <div className="flex justify-evenly p-3 border-t">
@@ -53,9 +77,10 @@ function InputBox() {
           <VideoCameraIcon className="h-7 text-red-500" />
           <p className="text-xs sm:text-sm xl:text-base">Live Video</p>
         </div>
-        <div className="inputIcon">
+        <div onClick={() => fileRef.current.click()} className="inputIcon">
           <CameraIcon className="h-7 text-green-400 " />
           <p className="text-xs sm:text-sm xl:text-base">Photo/Video</p>
+          <input ref={fileRef} onChange={addImageToPost} type="file" hidden />
         </div>
         <div className="inputIcon">
           <EmojiHappyIcon className="h-7 text-yellow-300" />
